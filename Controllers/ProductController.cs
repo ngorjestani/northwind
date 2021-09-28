426ca9f91478d01de8123536428b97a6cce6142d
Nick Gorjestani <nickg2794@hotmail.com>
2021-09-27 21:49:23 -0500

    Added Product View showing table of products per category

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -11,6 +11,10 @@ namespace Northwind.Controllers
             public ProductController(NorthwindContext db) => _northwindContext = db;
 
             public IActionResult Category() => View(_northwindContext.Categories.OrderBy(c => c.CategoryName));
+
+            public IActionResult Index(int id) => View(_northwindContext.Products
+                .Where(p => p.CategoryId == id && !p.Discontinued)
+                .OrderBy(p => p.ProductName));
     }
     
 }
