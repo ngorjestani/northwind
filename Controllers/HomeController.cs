f427eb170eab63f0d2c07f0f3fd9b210b4ca7c13
Nick Gorjestani <nickg2794@hotmail.com>
2021-10-01 21:04:25 -0500

    Added product list by category

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,10 +1,18 @@
 using System;
 using Microsoft.AspNetCore.Mvc;
+using Northwind.Models;
+using System.Linq;
 
 namespace Northwind.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index() => View();
+        // this controller depends on the NorthwindRepository
+        private NorthwindContext _northwindContext;
+        public HomeController(NorthwindContext db) => _northwindContext = db;
+
+        public ActionResult Index() => View(_northwindContext.Discounts
+            .Where(d => d.StartTime <= DateTime.Now && d.EndTime > DateTime.Now)
+            .Take(3));
     }
 }
--- Models/NorthwindContext.cs
+++ Models/NorthwindContext.cs
@@ -9,5 +9,7 @@ namespace Northwind.Models
 
         public DbSet<Category> Categories { get; set; }
         public DbSet<Product> Products { get; set; }
+        
+        public DbSet<Discount> Discounts { get; set; }
     }
 }
