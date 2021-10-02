e7567a827472f74fde78817494610325c437ad4d
Nick Gorjestani <nickg2794@hotmail.com>
2021-10-02 00:24:16 -0500

    Added toasts

--- wwwroot/js/client.js
+++ wwwroot/js/client.js
@@ -1,3 +1,27 @@
 $(function(){
-    //alert("Hello World");
+    var toast = new Audio('../media/toast.wav');
+    
+    $('.code').on('click', function(e) {
+        e.preventDefault();
+        
+        toast.pause();
+        
+        toast.currentTime = 0;
+        
+        toast.play();
+
+        let product = $(this).data('product');
+        let discountCode = $(this).data('code');
+
+        $('#product').html(product);
+        $('#code').html(discountCode);
+
+        $('#toast').toast({ autohide: false }).toast('show');
+    });
+});
+
+$(document).on('keydown', (e) => {
+    if (e.key === 'Escape') {
+        $('#toast').toast('hide');
+    }
 });
