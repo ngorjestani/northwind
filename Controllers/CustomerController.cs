62d7452b4e60c4cae041100ae1481ee46d1157d6
Nick Gorjestani <nickg2794@hotmail.com>
2021-10-05 22:25:40 -0500

    Added customer form

---
+++ Controllers/CustomerController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Northwind.Models; 
+using System.Linq;
+
+namespace Northwind.Controllers 
+{
+    
+    public class CustomerController : Controller
+    {
+        private NorthwindContext _northwindContext;
+        public CustomerController(NorthwindContext db) => _northwindContext = db;
+
+        public IActionResult Register() => View();
+    }
+    
+}
