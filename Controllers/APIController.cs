45b4c162308cb77c0e439762f02c6b41edc57688
Nick Gorjestani <nickg2794@hotmail.com>
2021-11-09 18:35:43 -0600

    API method to return all products from a category

--- Controllers/APIController.cs
+++ Controllers/APIController.cs
@@ -24,5 +24,11 @@ namespace Northwind.Controllers
         public IEnumerable<Product> GetDiscontinued(bool discontinued) => _northwindContext.Products
             .Where(p => p.Discontinued == discontinued)
             .OrderBy(p => p.ProductName);
+        
+        [HttpGet, Route("api/category/{CategoryId}/product")]
+        // returns all products in a specific category
+        public IEnumerable<Product> GetByCategory(int CategoryId) => _northwindContext.Products
+            .Where(p => p.CategoryId == CategoryId)
+            .OrderBy(p => p.ProductName);
     }
 }
