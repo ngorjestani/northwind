decdbfd1510ba6ade256b1840be1f712f2c17880
Nick Gorjestani <nickg2794@hotmail.com>
2021-11-09 18:16:50 -0600

    Create new controller - APIController

---
+++ Controllers/APIController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Northwind.Models;
+
+namespace Northwind.Controllers
+{
+    public class APIController : Controller
+    {
+        // this controller depends on the NorthwindRepository
+        private NorthwindContext _northwindContext;
+        public APIController(NorthwindContext db) => _northwindContext = db;
+
+        [HttpGet, Route("api/product")]
+        // returns all products
+        public IEnumerable<Product> Get() => _northwindContext.Products.OrderBy(p => p.ProductName);
+    }
+}
