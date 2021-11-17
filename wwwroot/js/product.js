178f8ffeeb9d46f5f9ff87ee3ce9cf4656208ceb
Nick Gorjestani <nickg2794@hotmail.com>
2021-11-16 18:37:31 -0600

    Move javascript to external file

---
+++ wwwroot/js/product.js
@@ -0,0 +1,33 @@
+﻿$(function () {
+    getProducts()
+    function getProducts() {
+        var id = $('#product_rows').data('id');
+        var discontinued = $('#Discontinued').prop('checked') ? "" : "/discontinued/false";
+        $.getJSON({
+            url: "../../api/category/" + id + "/product" + discontinued,
+            success: function (response, textStatus, jqXhr) {
+                $('#product_rows').html("");
+                for (var i = 0; i < response.length; i++){
+                    var css = response[i].discontinued ? " class=\"discontinued\"" : "";
+                    var row = "<tr" + css + ">"
+                        + "<td>" + response[i].productName + "</td>"
+                        + "<td class=\"text-right\">$" + response[i].unitPrice.toFixed(2) + "</td>"
+                        + "<td class=\"text-right\">" + response[i].unitsInStock + "</td>"
+                        + "</tr>";
+                    $('#product_rows').append(row);
+                }
+            },
+            error: function (jqXHR, textStatus, errorThrown) {
+                // log the error to the console
+                console.log("The following error occured: " + textStatus, errorThrown);
+            }
+        });
+    }
+    $('#CategoryId').on('change', function(){
+        $('#product_rows').data('id', $(this).val());
+        getProducts();
+    });
+    $('#Discontinued').on('change', function(){
+        getProducts();
+    });
+});
