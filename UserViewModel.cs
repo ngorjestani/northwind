97e9f5d519a9b058442417d79ad36c19017798a8
Connor <c.mccurrie@gmail.com>
2021-11-23 18:57:36 -0600

    Employee addition
    Added employee to northwind context and userviewmodel

---
+++ UserViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Northwind.Models
+{
+    public class CustomerWithPassword
+    {
+        public Customer Customer { get; set; }
+        [UIHint("password"), Required]
+        public string Password { get; set; }
+    }
+    public class LoginModel
+    {
+        [Required, UIHint("email")]
+        public string Email { get; set; }
+
+        [Required, UIHint("password")]
+        public string Password { get; set; }
+    }
+    //TODO add Employee class
+    public class EmployeeWithPassword{
+        [Required, UIHint("password")]
+        public string Email {get; set;}
+        public string Password {get; set;}
+    }
+}
