b75a4693d7c76301680aee6eb342fd69bdd56ee1
Nick Gorjestani <nickg2794@hotmail.com>
2021-11-26 19:20:37 -0600

    Added employee controller

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Northwind.Models;
+using System.Linq;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+
+namespace Northwind.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // this controller depends on the NorthwindRepository & the UserManager
+        private NorthwindContext _northwindContext;
+        private UserManager<AppUser> _userManager;
+        public EmployeeController(NorthwindContext db, UserManager<AppUser> usrMgr)
+        {
+            _northwindContext = db;
+            _userManager = usrMgr;
+        }
+        [Authorize(Roles = "northwind-employee")]
+        public IActionResult Account() => View(_northwindContext.Employees.FirstOrDefault(c => c.Email == User.Identity.Name));
+    }
+}
